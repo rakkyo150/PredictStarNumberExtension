6b728884187f2ff4a23fbbc165804e3154ad735e
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2022-03-30 07:06:00 +0900

    プレイヤーページのプレイ履歴にも対応

--- ScoreSaber.js
+++ ScoreSaber.js
@@ -39,107 +39,93 @@ function main(){
         else if(document.querySelector('.svelte-n29l0e')!=null){
             var mapCard=document.querySelector('.svelte-n29l0e');
             var beforeTag=mapCard.querySelector('.tag');
-            var difficulty=beforeTag.getAttribute("title");
             var rank=beforeTag.textContent;
             console.log("pass?");
             if(!rank.match(/★/)){
                 var hashElement=mapCard.querySelector('.text-muted');
                 let endpoint=`https://predictstarnumber.herokuapp.com/api/hash/${hashElement.textContent}`
-                fetch(endpoint,{
-                    mode: 'cors',
-                    method: 'GET'
-                }).then((response)=>{
-                    if(response.ok){
-                        return response.json();
-                    }
-                    else{
-                        throw new Error(response.statusText);
-                    }
-                }).then((json)=>{
-                    let predictStar;
-                    if(difficulty=="Easy"){
-                        predictStar='('+json.Easy+')★';
-                    }
-                    else if(difficulty=="Normal"){
-                        predictStar='('+json.Normal+')★';
-                    }
-                    else if(difficulty=="Hard"){
-                        predictStar='('+json.Hard+')★';
-                    }
-                    else if(difficulty=="Expert"){
-                        predictStar='('+json.Expert+')★';
-                    }
-                    else if(difficulty="Expert+"){
-                        predictStar='('+json.ExpertPlus+')★';
-                    }
-                    else{
-                        predictStar="?";
-                    }
-                    beforeTag.textContent=predictStar;
-                }).catch((err)=>{
-                    console.log(err);
-                    beforeTag.textContent="?";
-                })
+                SwapStar(endpoint,beforeTag)
             }
             clearInterval(jsInitCheckTimer);
         }
         // マップ一覧ページ
         else if(document.querySelector('.svelte-18crtdx')!=null){
             var mapCards=document.querySelectorAll('.song-container');
-            mapCards.forEach(mapCard => {
+            mapCards.forEach((mapCard) => {
                 console.log('pass');
                 console.log(mapCard);
                 var beforeTag=mapCard.querySelector('.tag');
-                var difficulty=beforeTag.getAttribute("title");
                 var rank=beforeTag.textContent;
                 if(!rank.match(/★/)){
                     var songInfo=mapCard.querySelector('.song-info');
                     var leaderboardIdElement=songInfo.querySelector('a');
                     var leaderboardId=leaderboardIdElement.getAttribute("href").replace('/leaderboard/','');
                     let endpoint=`https://predictstarnumber.herokuapp.com/api/leaderboardId/${leaderboardId}`;
-                    fetch(endpoint,{
-                        mode: 'cors',
-                        method: 'GET'
-                    }).then((response)=>{
-                        if(response.ok){
-                            return response.json();
-                        }
-                        else{
-                            throw new Error(response.statusText);
-                        }
-                    }).then((json)=>{
-                        let predictStar;
-                        if(difficulty=="Easy"){
-                            predictStar='('+json.Easy+')★';
-                        }
-                        else if(difficulty=="Normal"){
-                            predictStar='('+json.Normal+')★';
-                        }
-                        else if(difficulty=="Hard"){
-                            predictStar='('+json.Hard+')★';
-                        }
-                        else if(difficulty=="Expert"){
-                            predictStar='('+json.Expert+')★';
-                        }
-                        else if(difficulty="Expert+"){
-                            predictStar='('+json.ExpertPlus+')★';
-                        }
-                        // 念のため
-                        else{
-                            predictStar="?";
-                        }
-                        beforeTag.textContent=predictStar;
-                    }).catch((err)=>{
-                        console.log(err.message);
-                        beforeTag.textContent="?";
-                    })
+                    SwapStar(endpoint,beforeTag)
                 }
             });
             clearInterval(jsInitCheckTimer);
         }
         // プレイヤーページのマップ一覧ページ
         else if(document.querySelector('.svelte-1s2wgqz')!=null){
-            
+            var mapCards=document.querySelectorAll(".song-container");
+            console.log(mapCards[0]);
+            // mapCardが配列と似て非なるオブジェクトらしいので
+            mapCards.forEach((mapCard) => {
+                console.log('pass');
+                var beforeTag=mapCard.querySelector('.tag');
+                var rank=beforeTag.textContent;
+                if(!rank.match(/★/)){
+                    var songInfo=mapCard.querySelector('.song-info');
+                    var leaderboardIdElement=songInfo.querySelector('a');
+                    var leaderboardId=leaderboardIdElement.getAttribute("href").replace('/leaderboard/','');
+                    let endpoint=`https://predictstarnumber.herokuapp.com/api/leaderboardId/${leaderboardId}`;
+                    SwapStar(endpoint,beforeTag)
+                }
+            });
+            clearInterval(jsInitCheckTimer);
         }
     }
+}
+
+function SwapStar(endpoint,beforeTag){
+    var difficulty=beforeTag.getAttribute("title");
+    
+    fetch(endpoint,{
+        mode: 'cors',
+        method: 'GET'
+    }).then((response)=>{
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            throw new Error(response.statusText);
+        }
+    }).then((json)=>{
+        let predictStar;
+        console.log(json);
+        if(difficulty=="Easy"){
+            predictStar='('+json.Easy+')★';
+        }
+        else if(difficulty=="Normal"){
+            predictStar='('+json.Normal+')★';
+        }
+        else if(difficulty=="Hard"){
+            predictStar='('+json.Hard+')★';
+        }
+        else if(difficulty=="Expert"){
+            predictStar='('+json.Expert+')★';
+        }
+        else if(difficulty=="Expert+"){
+            predictStar='('+json.ExpertPlus+')★';
+        }
+        // 念のため
+        else{
+            predictStar="?";
+        }
+        beforeTag.textContent=predictStar;
+    }).catch((err)=>{
+        console.log(err.message);
+        beforeTag.textContent="?";
+    })
 }
