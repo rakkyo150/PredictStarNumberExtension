fcabaf609a458b675c5deef4520245dcd950856f
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2022-06-16 06:51:22 +0900

    自動整形

--- BeatSaver.js
+++ BeatSaver.js
@@ -1,12 +1,12 @@
-window.onload=start;
+window.onload = start;
 
-function start(){
+function start() {
     console.log("start");
     let lastUrl = ""
-    var main=document.querySelector('main');
+    var main = document.querySelector('main');
 
-    var mo = new MutationObserver(function() {
-        let url=location.href;
+    var mo = new MutationObserver(function () {
+        let url = location.href;
         console.log(url);
         if (url !== lastUrl) {
             lastUrl = url;
@@ -14,91 +14,91 @@ function start(){
         }
     });
     var config = {
-    childList: true
+        childList: true
     };
     mo.observe(main, config);
 }
 
-function core(){
-    let retryCount=0;
-    const maxRetry=5;
+function core() {
+    let retryCount = 0;
+    const maxRetry = 5;
     const jsInitCheckTimer = setInterval(jsLoaded, 1000);
     function jsLoaded() {
-        if(document.querySelector('.stats')==null){
+        if (document.querySelector('.stats') == null) {
             retryCount++;
-            if(retryCount ==maxRetry){
+            if (retryCount == maxRetry) {
                 console.log("５秒間必要な要素が見つからなかったので終了");
                 clearInterval(jsInitCheckTimer);
             }
         }
-        else if (document.querySelector('.stats')!=null){
+        else if (document.querySelector('.stats') != null) {
 
-            let oneClickUrl=document.querySelector('a[title="One-Click"]').getAttribute('href');
-            let id=oneClickUrl.replace('beatsaver://','')
-            if(id==null){
+            let oneClickUrl = document.querySelector('a[title="One-Click"]').getAttribute('href');
+            let id = oneClickUrl.replace('beatsaver://', '')
+            if (id == null) {
                 console.log("idを取得できませんでした");
             }
-            else{
-                let endpoint=`https://predictstarnumber.herokuapp.com/api/id/${id}`
-                fetch(endpoint,{
+            else {
+                let endpoint = `https://predictstarnumber.herokuapp.com/api/id/${id}`
+                fetch(endpoint, {
                     mode: 'cors',
                     method: 'GET'
-                }).then((response)=>{
-                    if(response.ok){
+                }).then((response) => {
+                    if (response.ok) {
                         return response.json();
                     }
-                    else{
+                    else {
                         throw new Error();
                     }
-                }).then((json)=>{
-                    var mapStats=document.querySelector('.mapstats');
-                    var difficultyList =mapStats.querySelectorAll('.list-group-item');
-                    
-                    let passPredict=false;
-                    if(difficultyList==null){
+                }).then((json) => {
+                    var mapStats = document.querySelector('.mapstats');
+                    var difficultyList = mapStats.querySelectorAll('.list-group-item');
+
+                    let passPredict = false;
+                    if (difficultyList == null) {
                         console.log("マップの種類を取得できませんでした");
                     }
-                    else{
-                        difficultyList.forEach(function (difficultyItem){
+                    else {
+                        difficultyList.forEach(function (difficultyItem) {
                             let starNumber;
-                            if(difficultyItem.querySelector('.diff-stars')==null){
-                                if(difficultyItem.querySelector('img[title~="Expert+"]')!=null){
-                                    starNumber=json.ExpertPlus;
+                            if (difficultyItem.querySelector('.diff-stars') == null) {
+                                if (difficultyItem.querySelector('img[title~="Expert+"]') != null) {
+                                    starNumber = json.ExpertPlus;
                                     console.log(starNumber);
                                 }
-                                else if(difficultyItem.querySelector('img[title~="Expert"]')!=null){
-                                    starNumber=json.Expert;
+                                else if (difficultyItem.querySelector('img[title~="Expert"]') != null) {
+                                    starNumber = json.Expert;
                                     console.log(starNumber);
                                 }
-                                else if(difficultyItem.querySelector('img[title~="Hard"]')!=null){
-                                    starNumber=json.Hard;
+                                else if (difficultyItem.querySelector('img[title~="Hard"]') != null) {
+                                    starNumber = json.Hard;
                                     console.log(starNumber);
-                                }else if(difficultyItem.querySelector('img[title~="Normal"]')!=null){
-                                    starNumber=json.Normal;
-                                    console.log(starNumber);
-                                }
-                                else if(difficultyItem.querySelector('img[title~="Easy"]')!=null){
-                                    starNumber=json.Easy;
-                                    console.log(starNumber);
-                                // 念のため
-                                }else{
-                                    starNumber="?";
+                                } else if (difficultyItem.querySelector('img[title~="Normal"]') != null) {
+                                    starNumber = json.Normal;
                                     console.log(starNumber);
                                 }
-                                var stats=difficultyItem.querySelector('.stats');
+                                else if (difficultyItem.querySelector('img[title~="Easy"]') != null) {
+                                    starNumber = json.Easy;
+                                    console.log(starNumber);
+                                    // 念のため
+                                } else {
+                                    starNumber = "?";
+                                    console.log(starNumber);
+                                }
+                                var stats = difficultyItem.querySelector('.stats');
                                 console.log(stats);
-                                
-                                var starSpan=document.createElement('span');
+
+                                var starSpan = document.createElement('span');
                                 starSpan.classList.add('diff-stars');
-                                starSpan.textContent='('+starNumber+')';
+                                starSpan.textContent = '(' + starNumber + ')';
                                 starSpan.style.margin = '0px auto';
                                 starSpan.style.paddingLeft = '5px';
                                 starSpan.style.paddingRight = '5px';
                                 starSpan.style.paddingTop = '0px';
                                 starSpan.style.paddingBottom = '0px';
                                 starSpan.style.textAlign = 'center';
-                                
-                                var starI=document.createElement('i');
+
+                                var starI = document.createElement('i');
                                 starI.classList.add('fas');
                                 starI.classList.add('fa-star')
                                 starSpan.prepend(starI);
@@ -107,7 +107,7 @@ function core(){
                             }
                         })
                     }
-                }).catch((err)=>{
+                }).catch((err) => {
                     console.log(err);
                 })
             }
--- ScoreSaber.js
+++ ScoreSaber.js
@@ -1,14 +1,14 @@
-window.onload=start;
+window.onload = start;
 
-function start(){
+function start() {
 
-    let lastUrl = ""; 
+    let lastUrl = "";
     console.log(lastUrl)
 
-    var body=document.querySelector('body');
+    var body = document.querySelector('body');
 
-    var mo = new MutationObserver(function() {
-        let url=location.href;
+    var mo = new MutationObserver(function () {
+        let url = location.href;
         console.log(url);
         if (url !== lastUrl) {
             lastUrl = url;
@@ -16,49 +16,49 @@ function start(){
         }
     });
     var config = {
-    subtree:true,
-    attributes:true,
+        subtree: true,
+        attributes: true,
     };
     mo.observe(body, config);
 }
 
-function main(){
+function main() {
     console.log("main fired");
-    let retryCount=0;
-    const maxRetry=5;
+    let retryCount = 0;
+    const maxRetry = 5;
     const jsInitCheckTimer = setInterval(jsLoaded, 1000);
     function jsLoaded() {
-        if(document.querySelector('.media-content.is-clipped')==null &&
-        document.querySelector('.song-container')==null){
+        if (document.querySelector('.media-content.is-clipped') == null &&
+            document.querySelector('.song-container') == null) {
             retryCount++;
-            if(retryCount ==maxRetry){
+            if (retryCount == maxRetry) {
                 console.log("５秒間必要な要素が見つからなかったので終了");
                 clearInterval(jsInitCheckTimer);
             }
         }
         // マップのリーダーボードorランクリクエストの譜面の固有のページ
-        else if(document.querySelector('.media-content.is-clipped')!=null){
+        else if (document.querySelector('.media-content.is-clipped') != null) {
             PredictStarNumberForLeaderboard(jsInitCheckTimer);
         }
         // プレイヤーページのマップ一覧ページorマップ一覧ページ
-        else if(document.querySelector('.song-container')!=null){
+        else if (document.querySelector('.song-container') != null) {
             PredictStarNumberForMapList(jsInitCheckTimer);
         }
     }
 }
 
 function PredictStarNumberForMapList(jsInitCheckTimer) {
-    const mapListUrl='https://scoresaber.com/leaderboards';
-    const usersMapListUrl='https://scoresaber.com/u';
-    if(!location.href.includes(mapListUrl) && !location.href.includes(usersMapListUrl)) return;
-    
+    const mapListUrl = 'https://scoresaber.com/leaderboards';
+    const usersMapListUrl = 'https://scoresaber.com/u';
+    if (!location.href.includes(mapListUrl) && !location.href.includes(usersMapListUrl)) return;
+
     var mapCards = document.querySelectorAll('.song-container');
     mapCards.forEach((mapCard) => {
         console.log('pass');
         console.log(mapCard);
         var beforeTag = mapCard.querySelector('.tag');
         var rank = beforeTag.textContent;
-        
+
         if (!rank.includes('★') || rank.includes(')★')) {
             var songInfo = mapCard.querySelector('.song-info');
             var leaderboardIdElement = songInfo.querySelector('a');
@@ -71,15 +71,15 @@ function PredictStarNumberForMapList(jsInitCheckTimer) {
 }
 
 function PredictStarNumberForLeaderboard(jsInitCheckTimer) {
-    const leaderboardUrl='https://scoresaber.com/leaderboard';
-    const requestUrl='https://scoresaber.com/ranking/request';
-    if(!location.href.includes(leaderboardUrl) && !location.href.includes(requestUrl)) return;
+    const leaderboardUrl = 'https://scoresaber.com/leaderboard';
+    const requestUrl = 'https://scoresaber.com/ranking/request';
+    if (!location.href.includes(leaderboardUrl) && !location.href.includes(requestUrl)) return;
 
     var mapCard = document.querySelector('.media-content.is-clipped');
     var beforeTag = mapCard.querySelector('.tag');
     var rank = beforeTag.textContent;
     console.log("pass?");
-    
+
 
     if (!rank.includes('★') || rank.includes(')★')) {
         var titleElement = mapCard.querySelector('.title');
@@ -91,44 +91,44 @@ function PredictStarNumberForLeaderboard(jsInitCheckTimer) {
     clearInterval(jsInitCheckTimer);
 }
 
-function SwapStar(endpoint,beforeTag){
-    var difficulty=beforeTag.getAttribute("title");
-    
-    fetch(endpoint,{
+function SwapStar(endpoint, beforeTag) {
+    var difficulty = beforeTag.getAttribute("title");
+
+    fetch(endpoint, {
         mode: 'cors',
         method: 'GET'
-    }).then((response)=>{
-        if(response.ok){
+    }).then((response) => {
+        if (response.ok) {
             return response.json();
         }
-        else{
+        else {
             throw new Error(response.statusText);
         }
-    }).then((json)=>{
+    }).then((json) => {
         let predictStar;
         console.log(json);
-        if(difficulty=="Easy"){
-            predictStar='('+json.Easy+')★';
+        if (difficulty == "Easy") {
+            predictStar = '(' + json.Easy + ')★';
         }
-        else if(difficulty=="Normal"){
-            predictStar='('+json.Normal+')★';
+        else if (difficulty == "Normal") {
+            predictStar = '(' + json.Normal + ')★';
         }
-        else if(difficulty=="Hard"){
-            predictStar='('+json.Hard+')★';
+        else if (difficulty == "Hard") {
+            predictStar = '(' + json.Hard + ')★';
         }
-        else if(difficulty=="Expert"){
-            predictStar='('+json.Expert+')★';
+        else if (difficulty == "Expert") {
+            predictStar = '(' + json.Expert + ')★';
         }
-        else if(difficulty=="Expert+"){
-            predictStar='('+json.ExpertPlus+')★';
+        else if (difficulty == "Expert+") {
+            predictStar = '(' + json.ExpertPlus + ')★';
         }
         // 念のため
-        else{
-            predictStar="?";
+        else {
+            predictStar = "?";
         }
-        beforeTag.textContent=predictStar;
-    }).catch((err)=>{
+        beforeTag.textContent = predictStar;
+    }).catch((err) => {
         console.log(err.message);
-        beforeTag.textContent="?";
+        beforeTag.textContent = "?";
     })
 }
