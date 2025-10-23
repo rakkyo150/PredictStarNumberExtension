0829f53adfb9f27d7af0f8ec43c0a01d53429772
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2025-10-24 05:40:09 +0900

    モデルをリリースから取得するように変更

--- src-ts/modelGetter.ts
+++ src-ts/modelGetter.ts
@@ -1,6 +1,6 @@
 export async function getModel() {
   const modelAssetEndpoint =
-      "https://raw.githubusercontent.com/rakkyo150/PredictStarNumberHelper/master/model.onnx";
+      "https://github.com/rakkyo150/PredictStarNumberHelper/releases/latest/download/model.onnx";
   const response = await fetch(modelAssetEndpoint);
   const buf = await response.arrayBuffer();
   return new Uint8Array(buf);
